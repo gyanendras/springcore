593d6c7a0f84ccaefe7b3720399f0249a42a8727
Singh <t-Gyanendra@cognizant.com>
2020-01-24 15:37:15 +0530

    modifed springcore

--- src/main/java/com/cts/springcore/SpringcoreApplication.java
+++ src/main/java/com/cts/springcore/SpringcoreApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringcoreApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World !");
 		SpringApplication.run(SpringcoreApplication.class, args);
 	}
 
